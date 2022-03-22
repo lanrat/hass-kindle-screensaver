1278c6ed79b12bc328899f4f5c9267ba546a71f2
Ian Foster <ian@vorsk.com>
2022-03-21 22:31:56 -0700

    my config

--- extensions/homeassistant/config.sh
+++ extensions/homeassistant/config.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-INTERVAL=60                             # (sec) - how often to update the script
-IMAGE_URI="http://example.org/test.png" # URL of image to fetch. Keep in mind that the Kindle 4 does not support SSL/TLS requests
+#KINDLE_NAME="KA"
+KINDLE_NAME="KTB"
+INTERVAL=500                             # (sec) - how often to update the script
+#IMAGE_URI="http://192.168.2.4:5000/2.png" # URL of image to fetch. Keep in mind that the Kindle 4 does not support SSL/TLS requests
+IMAGE_URI="http://192.168.2.4:5000/3.png"
 CLEAR_SCREEN_BEFORE_RENDER=0            # If "1", then the screen is completely cleared before rendering the newly fetched image to avoid "shadows".
 INTERVAL_ON_ERROR=30                    # In case of errors, the device waits this long until the next loop.
 BATTERYALERT=15                         # if the battery level is equal to or below this threshold, a info will be displayed
 BATTERYLOW=5                            # if the battery level is equal to or below this threshold, the "please charge" image will be shown and the device will sleep for a long time until it checks again (or boots up and starts the script again)
 BATTERYSLEEP=3600                       # 1 day sleep time when Battery Level is equal or below the "BATTERYLOW" threshold, see above.
 PINGHOST="www.google.com"               # which domain (or IP) to ping to check internet connectivity.
-ROUTERIP="192.168.0.1"                  # router gateway IP. The Kindle appears to sometimes forget the gateway's IP and we need to set this manually.
+ROUTERIP="192.168.2.1"                  # router gateway IP. The Kindle appears to sometimes forget the gateway's IP and we need to set this manually.
 LOGGING=1                               # if enabled, the script logs into a file
-DELAY_BEFORE_SUSPEND=10                 # seconds to wait between drawing image and suspending. This gives you time to SSH into your device if it's inside the photo frame and stop the daemon
-RESTART_POWERD_THRESHOLD=50 # restart powerd if battery percentage is below this value, if a power source is connected and the charging current is negative
+# I think setting this too high can cause the top bar to appear on the KT (default 10)
+DELAY_BEFORE_SUSPEND=5                 # seconds to wait between drawing image and suspending. This gives you time to SSH into your device if it's inside the photo frame and stop the daemon
+RESTART_POWERD_THRESHOLD=50             # restart powerd if battery percentage is below this value, if a power source is connected and the charging current is negative
+
+
+#REFRESH_EVERY=5 # how often to completely refresh the screen
 
 NAME=homeassistant
 NAMEOLD=homeassistant_old
